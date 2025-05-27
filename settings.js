c4a108c912e50040ca3ad7af8cad9872c5388675
Dasun200710 <pk4245477@gmail.com>
2025-05-27 15:10:37 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,25 @@
+module.exports = {
+    audioUrl: 'https://github.com/drsde/send-voice/raw/refs/heads/main/public/DEXTER.mp3',
+    photoUrl: 'https://i.ibb.co/m59wcpJX/20250214-011325.jpg',
+    generalMessage: `
+╭────────────⊶
+│ ɪ ᴀᴍ ʀᴇᴀʟ ᴅᴇxᴛᴇʀ
+╰────────────⊶
+
+╭────────────⊶
+│ ○ ɴᴀᴍᴇ │ DASSA
+│ ○ ꜰʀᴏᴍ │ MAHO
+│ ○ ᴀɢᴇ │ +17
+╰────────────⊶
+
+╭────────────⊶
+│ ❮ මාව save කර ගන්න ❯
+│ "ඔයාව SAVE"
+│ ❮ AUTO REPLY ❯
+╰────────────⊶
+
+❮ ꜱᴀᴠᴇ නම් අනිවරෙන් කියන්න ❯
+❮ STATUS VIEWERS වලට අවේ ❯
+    `,
+    groupLink: 'https://chat.whatsapp.com/EaT8qBl0Atg9IaiBI1hAeC'
+};
